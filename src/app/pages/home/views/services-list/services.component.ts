66be807745f4aa4631775f7d8ef9feac3134fc1b
mamdouh <mo.mamdouh.saad@gmail.com>
2020-05-20 11:56:53 +0200

    last touches

--- src/app/pages/home/views/services-list/services.component.ts
+++ src/app/pages/home/views/services-list/services.component.ts
@@ -10,18 +10,18 @@ export class ServicesListComponent implements OnInit {
   services: Service[] = [
     {
     iconUrl:'assets/svg/001-process.svg',
-    title:'SOCIAL MEDIA MANAGEMENT',
+    title:'SOCIAL MEDIA ',
     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
    },
    {
     iconUrl:'assets/svg/002-social-media.svg',
-    title:'SOCIAL GROWTH CAMPAIGN',
+    title:'SOCIAL GROWTH ',
     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
    },
    {
     iconUrl:'assets/svg/004-social-media-1.svg',
     title:'BRANDING & MEDIA',
-    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
+    description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit,  sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
    },
    {
     iconUrl:'assets/svg/007-analysis.svg',
@@ -30,12 +30,12 @@ export class ServicesListComponent implements OnInit {
    },
    {
     iconUrl:'assets/svg/004-social-media-1.svg',
-    title:'SOCIAL MEDIA PAID ADVERTISING ',
+    title:'SOCIAL MEDIA PAID  ',
     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
    },
    {
     iconUrl:'assets/svg/008-computer.svg',
-    title:'CONTENT MARKETING SERVICES',
+    title:'CONTENT MARKETING ',
     description:'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur'
    },
    
